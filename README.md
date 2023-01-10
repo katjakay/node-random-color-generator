9c288af99263cf5d4effdf4ca8b2b6769137c022
Katja Sabando <katja.sabando@gmail.com>
2023-01-10 16:47:20 +0100

    New updates

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Node Random Color Generator
+
+Generate a random color in the command line
+
+## Usage
+
+When user enters in the command line:
+
+````bash
+node index.js
+31x9 #
+hex code e.g #ff000
+```bash
+
+Generate random colors:
+
+```bash
+accept request for
+hue e.g green or red or blue
+luminosity e.g light or dark
+```bash
+
+
+
+````
---
+++ index.js
@@ -0,0 +1,41 @@
+import chalk from 'chalk';
+import randomColor from 'randomcolor';
+
+const lum = process.argv[3];
+const col = process.argv[2];
+
+const color = randomColor({
+  luminosity: lum,
+  hue: col,
+});
+function colorAndLog(str) {
+  console.log(chalk.hex(color).bold(str));
+}
+
+//LONG VERSION
+// colorAndLog("##############################");
+// colorAndLog("##############################");
+// colorAndLog("##############################");
+// colorAndLog("########              ########");
+// colorAndLog(`########   ${color}    ########`);
+// colorAndLog("########              ########");
+// colorAndLog("##############################");
+// colorAndLog("##############################");
+// colorAndLog("##############################");
+
+//SHORT VERSION
+colorAndLog(`##############################
+##############################
+##############################
+########              ########
+########   ${color}    ########
+########              ########
+##############################
+##############################
+##############################`);
+
+// this generates random HEX code
+// const color = randomColor();
+
+// this colors the str
+// log(chalk.hex('color').(str));
