3471ce0b87cf83aba4c27d490ef8b98e3bfb7b57
Katja Sabando <katja.sabando@gmail.com>
2023-01-17 23:39:28 +0100

    Clean code and add comments

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ import randomColor from 'randomcolor';
 const lum = process.argv[3];
 const col = process.argv[2];
 
+// 1. randomcolor npm generates random HEX code
+// 2. It also defines luminosity & hue -> user input
 const color = randomColor({
   luminosity: lum,
   hue: col,
 });
 
+// 3. chalk npm colorized a variable, value etc.
 console.log(
   chalk.hex(color)(
     `  ##############################
@@ -22,9 +25,3 @@ console.log(
   ##############################`,
   ),
 );
-
-// this generates random HEX code
-// const color = randomColor();
-
-// this colors the str
-// log(chalk.hex('color')(str));
